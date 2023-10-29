38452ecf79fb3a99386df34e2fb6a2ce9eb2a200
johntvu <120274177+johnvu124@users.noreply.github.com>
2023-10-29 14:03:49 -0400

    Add function to get Computer Choice for RPS

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 // Create a function of computerChoice that randomly selects between rps
+function getComputerChoice() {
+    number = Math.floor(Math.random() * 3)
+
+    if (number === 0) {
+        return "Rock";
+    }
+
+        else if (number === 1) {
+            return "Paper"
+        }
+
+        else {
+            return "Scissors"
+        }
+}
+
+console.log(getComputerChoice())
 // Ask the user for an input of rps, not case sensitive
 // check for the winner
 // print the winner and the choices
