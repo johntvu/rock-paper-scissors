c4c6e829645901d5cb8af09236bd9f21ec9ccbcc
johntvu <120274177+johnvu124@users.noreply.github.com>
2023-10-29 14:03:00 -0400

    Add HTML and JavaScript initial files, Add psuedocode

---
+++ main.js
@@ -0,0 +1,7 @@
+// Create a function of computerChoice that randomly selects between rps
+// Ask the user for an input of rps, not case sensitive
+// check for the winner
+// print the winner and the choices
+// loop through game 5 times
+// keep the score
+// report loser or winner
